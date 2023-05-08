129d3c2439d7dce21d22c81cdb3e0e8a0d28ed7f
CstDc <129838965+CstDc@users.noreply.github.com>
2023-05-08 10:49:48 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,91 +1,31 @@
-import json
-import logging
+import tkinter as tk
+from tkinter import messagebox
 
-from django.http import JsonResponse
-from django.shortcuts import render
-from wxcloudrun.models import Counters
+# 创建窗口对象
+window = tk.Tk()
+window.title("输入名字和密码")
 
+# 创建标签和输入框
+name_label = tk.Label(window, text="名字:")
+name_label.pack()
+name_entry = tk.Entry(window)
+name_entry.pack()
 
-logger = logging.getLogger('log')
+password_label = tk.Label(window, text="密码:")
+password_label.pack()
+password_entry = tk.Entry(window, show="*")
+password_entry.pack()
 
+# 定义按钮点击事件处理函数
+def on_submit():
+    name = name_entry.get()
+    password = password_entry.get()
+    message = "您输入的名字是：" + name + "\n您输入的密码是：" + password
+    messagebox.showinfo("提示", message)
 
-def index(request, _):
-    """
-    获取主页
+# 创建提交按钮
+submit_button = tk.Button(window, text="提交", command=on_submit)
+submit_button.pack()
 
-     `` request `` 请求对象
-    """
-
-    return render(request, 'index.html')
-
-
-def counter(request, _):
-    """
-    获取当前计数
-
-     `` request `` 请求对象
-    """
-
-    rsp = JsonResponse({'code': 0, 'errorMsg': ''}, json_dumps_params={'ensure_ascii': False})
-    if request.method == 'GET' or request.method == 'get':
-        rsp = get_count()
-    elif request.method == 'POST' or request.method == 'post':
-        rsp = update_count(request)
-    else:
-        rsp = JsonResponse({'code': -1, 'errorMsg': '请求方式错误'},
-                            json_dumps_params={'ensure_ascii': False})
-    logger.info('response result: {}'.format(rsp.content.decode('utf-8')))
-    return rsp
-
-
-def get_count():
-    """
-    获取当前计数
-    """
-
-    try:
-        data = Counters.objects.get(id=1)
-    except Counters.DoesNotExist:
-        return JsonResponse({'code': 0, 'data': 0},
-                    json_dumps_params={'ensure_ascii': False})
-    return JsonResponse({'code': 0, 'data': data.count},
-                        json_dumps_params={'ensure_ascii': False})
-
-
-def update_count(request):
-    """
-    更新计数，自增或者清零
-
-    `` request `` 请求对象
-    """
-
-    logger.info('update_count req: {}'.format(request.body))
-
-    body_unicode = request.body.decode('utf-8')
-    body = json.loads(body_unicode)
-
-    if 'action' not in body:
-        return JsonResponse({'code': -1, 'errorMsg': '缺少action参数'},
-                            json_dumps_params={'ensure_ascii': False})
-
-    if body['action'] == 'inc':
-        try:
-            data = Counters.objects.get(id=1)
-        except Counters.DoesNotExist:
-            data = Counters()
-        data.id = 1
-        data.count += 1
-        data.save()
-        return JsonResponse({'code': 0, "data": data.count},
-                    json_dumps_params={'ensure_ascii': False})
-    elif body['action'] == 'clear':
-        try:
-            data = Counters.objects.get(id=1)
-            data.delete()
-        except Counters.DoesNotExist:
-            logger.info('record not exist')
-        return JsonResponse({'code': 0, 'data': 0},
-                    json_dumps_params={'ensure_ascii': False})
-    else:
-        return JsonResponse({'code': -1, 'errorMsg': 'action参数错误'},
-                    json_dumps_params={'ensure_ascii': False})
+# 运行主循环
+window.mainloop()
